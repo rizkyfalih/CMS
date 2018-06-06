ee0965dbf663f3ffc606d2c8f30861378974b657
rizkyfalih <rizkyfalihpridyandhika@ymail.com>
2018-06-06 15:12:15 +0700

    Added Widget Module

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -4,12 +4,12 @@
 <?php session_start(); ?>
 
 <?php
+
     if(!isset($_SESSION['user_role'])){
         header("Location: ../index.php");
     
     }
-
-
+    
 ?>
 
 
@@ -41,6 +41,8 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
 </head>
 
