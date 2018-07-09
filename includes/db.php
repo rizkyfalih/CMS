73b81248f1e0f11a489697c8d85c85ffd692bdbe
rizkyfalih <rizkyfalihpridyandhika@ymail.com>
2018-07-10 02:12:42 +0700

    Updated database

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,18 @@
 <?php
 
-$db['db_host'] = "localhost";
-$db['db_user'] = "root";
-$db['db_pass'] = "";
-$db['db_name'] = "cms";
+// Local Database
+// $db['db_host'] = "localhost";
+// $db['db_user'] = "root";
+// $db['db_pass'] = "";
+// $db['db_name'] = "cms";
+
+// Heroku Clear DB
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$db['db_host'] = $url["host"];
+$db['db_user'] = $url["user"];
+$db['db_pass'] = $url["pass"];
+$db['db_name'] = substr($url["path"], 1);
 
 foreach ($db as $key => $value) {
   define(strtoupper($key), $value);
