71cb0e02ea6afb06a3a1d4a6801955afcbb3c35c
rizkyfalih <rizkyfalihpridyandhika@ymail.com>
2018-07-24 19:59:26 +0700

    updated active link and login bar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -19,19 +19,28 @@
 
     <!-- Login -->
     <div class="well">
-        <h4>Login</h4>
-        <form action="includes/login.php" method="post">
-        <div class="form-group">
-            <input name="username" type="text" class="form-control" placeholder = "Enter Username">
-        </div>
-        <div class="input-group">
-            <input name="password" type="password" class="form-control" placeholder = "Enter Password">
-            <span class="input-group-btn">
-                <button class="btn btn-primary" name="login" type="submit">Submit</button>
-            </span>
-        </div>
-        </form><!--search form-->
-        <!-- /.input-group -->
+        <?php if(isset($_SESSION['user_role'])):?>
+
+            <h4>Logged in as <?php echo $_SESSION['username'];?></h4> 
+            <a href="includes/logout.php" class="btn btn-primary">Logout</a>
+        <?php else:?>
+
+            <h4>Login</h4>
+            <form action="includes/login.php" method="post">
+            <div class="form-group">
+                <input name="username" type="text" class="form-control" placeholder = "Enter Username">
+            </div>
+            <div class="input-group">
+                <input name="password" type="password" class="form-control" placeholder = "Enter Password">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" name="login" type="submit">Submit</button>
+                </span>
+            </div>
+            </form><!--search form-->
+            <!-- /.input-group -->
+
+        <?php endif;?>
+
     </div>
 
 
@@ -64,7 +73,5 @@
         <!-- /.row -->
     </div>
 
-    <!-- Side Widget Well -->
-    <?php include "widget.php"; ?>
 
 </div>
