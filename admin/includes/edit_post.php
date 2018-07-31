b9ffcfdd4336c3915f2a356e19c7af1bc3c18001
rizkyfalih <rizkyfalihpridyandhika@ymail.com>
2018-07-31 07:20:50 +0700

    fixed duplicate category, bug in our edit post, clonning empty fields and table shifting

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,64 +1,66 @@
 <?php
 
-    if(isset($_GET['p_id'])){
-        $the_post_id = $_GET['p_id'];
-    }
+  if(isset($_GET['p_id'])){
+    $the_post_id = $_GET['p_id'];
+  }
 
-    $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
-    $select_posts_by_id= mysqli_query($connection, $query);
+  $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+  $select_posts_by_id= mysqli_query($connection, $query);
 
-    while($row = mysqli_fetch_assoc($select_posts_by_id)){
-        $post_id = $row['post_id'];
-        $post_user = $row['post_user'];
-        $post_title = $row['post_title'];
-        $post_category_id = $row['post_category_id'];
-        $post_status = $row['post_status'];
-        $post_image = $row['post_image'];
-        $post_content = $row['post_content'];
-        $post_tags = $row['post_tags'];
-        $post_comment_count = $row['post_comment_count'];
-        $post_date = $row['post_date'];
+  while($row = mysqli_fetch_assoc($select_posts_by_id)){
+    $post_id = $row['post_id'];
+    $post_user = $row['post_user'];
+    $post_title = $row['post_title'];
+    $post_category_id = $row['post_category_id'];
+    $post_status = $row['post_status'];
+    $post_image = $row['post_image'];
+    $post_content = $row['post_content'];
+    $post_tags = $row['post_tags'];
+    $post_comment_count = $row['post_comment_count'];
+    $post_date = $row['post_date'];
     
+  }
+
+  if(isset($_POST['update_post'])){
+    $post_title = $_POST['title'];
+    $post_user = $_POST['post_user'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_tags = $_POST['post_tags'];
+    $post_content = $_POST['post_content'];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    if(empty($post_image)){
+      $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+      $select_image = mysqli_query($connection, $query);
+      while($row = mysqli_fetch_array($select_image)){
+        $post_image = $row['post_image'];
+      }
     }
 
-    if(isset($_POST['update_post'])){
-        $post_title = $_POST['title'];
-        $post_user = $_POST['post_user'];
-        $post_category_id = $_POST['post_category_id'];
-        $post_status = $_POST['post_status'];
-        $post_image = $_FILES['image']['name'];
-        $post_image_temp = $_FILES['image']['tmp_name'];
-        $post_tags = $_POST['post_tags'];
-        $post_content = $_POST['post_content'];
+    $post_title = mysqli_real_escape_string($connection, $post_title);
 
-        move_uploaded_file($post_image_temp, "../images/$post_image");
+    $query = "UPDATE posts SET ";
+    $query .= "post_title = '{$post_title}', ";
+    $query .= "post_category_id = {$post_category_id}, ";
+    $query .= "post_date = now(), ";
+    $query .= "post_user = '{$post_user}', ";
+    $query .= "post_status = '{$post_status}', ";
+    $query .= "post_tags = '{$post_tags}', ";
+    $query .= "post_content = '{$post_content}', ";
+    $query .= "post_image = '{$post_image}' ";
+    $query .= "WHERE post_id = {$the_post_id} ";
 
-        if(empty($post_image)){
-            $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
-            $select_image = mysqli_query($connection, $query);
-            while($row = mysqli_fetch_array($select_image)){
-                $post_image = $row['post_image'];
-            }
-        }
+    $update_post = mysqli_query($connection, $query);
 
-        $query = "UPDATE posts SET ";
-        $query .= "post_title = '{$post_title}', ";
-        $query .= "post_category_id = '{$post_category_id}', ";
-        $query .= "post_date = now(), ";
-        $query .= "post_user = '{$post_user}', ";
-        $query .= "post_status = '{$post_status}', ";
-        $query .= "post_tags = '{$post_tags}', ";
-        $query .= "post_content = '{$post_content}', ";
-        $query .= "post_image = '{$post_image}' ";
-        $query .= "WHERE post_id = {$the_post_id} ";
+    confirmQuery($update_post);
 
-        $update_post = mysqli_query($connection, $query);
+    echo "<p class='bg-success'>Post Updated. <a href='../post.php?p_id={$the_post_id}'> View Post</a> or <a href='posts.php'> Edit More posts</a></p>";
 
-        confirmQuery($update_post);
-
-        echo "<p class='bg-success'>Post Updated. <a href='../post.php?p_id={$the_post_id}'> View Post</a> or <a href='posts.php'> Edit More posts</a></p>";
-
-    }
+  }
 
 ?>
 
@@ -75,17 +77,22 @@
     <select name="post_category_id" id="" class="form-control">
 
         <?php 
-            $query = "SELECT * FROM categories";
-            $select_categories = mysqli_query($connection, $query);
+          $query = "SELECT * FROM categories";
+          $select_categories = mysqli_query($connection, $query);
 
-            confirmQuery($select_categories);
+          confirmQuery($select_categories);
             
-            while($row = mysqli_fetch_assoc($select_categories)){
-                $cat_id = $row['cat_id'];
-                $cat_title = $row['cat_title'];
-                echo "<option value='$cat_id'>{$cat_title}</option>";
+          while($row = mysqli_fetch_assoc($select_categories)){
+            $cat_id = $row['cat_id'];
+            $cat_title = $row['cat_title'];
 
+            if($cat_id == $post_category_id){
+              echo "<option selected value='$cat_id'>{$cat_title}</option>";
+            } else {
+              echo "<option value='$cat_id'>{$cat_title}</option>";
             }
+
+          }
         
         ?>
     </select>
@@ -99,19 +106,19 @@
   <div class="form-group">
     <label for="users">Users</label>
     <select class="form-control" name="post_user" id="">
-    <?php echo "<option value='$post_user'>{$post_user}</option>"; ?>
-      <?php 
-        $query = "SELECT * FROM users";
-        $select_users = mysqli_query($connection, $query);
+      <?php echo "<option value='$post_user'>{$post_user}</option>"; ?>
+        <?php 
+          $query = "SELECT * FROM users";
+          $select_users = mysqli_query($connection, $query);
 
-        confirmQuery($select_users);
-            
-        while($row = mysqli_fetch_assoc($select_users)){
-          $user_id = $row['user_id'];
-          $username = $row['username'];
-          echo "<option value='$username'>{$username}</option>";
-        }
-      ?>
+          confirmQuery($select_users);
+              
+          while($row = mysqli_fetch_assoc($select_users)){
+            $user_id = $row['user_id'];
+            $username = $row['username'];
+            echo "<option value='$username'>{$username}</option>";
+          }
+        ?>
     </select>
   </div>
 
@@ -136,7 +143,7 @@
   <div class="form-group">
     <img width="100"  src="../images/<?php echo $post_image; ?>" alt="">
     <input type="file" name="image">
-    </div>
+  </div>
 
   <div class="form-group">
     <label for="post_tags">Post Tags</label>
@@ -145,7 +152,7 @@
 
   <div class="form-group">
     <label for="post_content">Post Content</label>
-    <textarea class="form-control" name="post_content" rows="10" cols="30"><?php echo $post_content; ?></textarea>
+    <textarea class="form-control" name="post_content" rows="10" cols="30"><?php echo str_replace('\r\n','</br>',$post_content) ; ?></textarea>
   </div>
 
   <div class="form-group">
