79b357bf67c04bf2c82de1ecfa57e196fa786fc8
rizkyfalih <rizkyfalihpridyandhika@ymail.com>
2018-08-04 10:12:38 +0700

    updated email

--- email.php
+++ email.php
@@ -42,7 +42,7 @@ require 'vendor/autoload.php';
         $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
         try {
             //Server settings
-            $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+            // $mail->SMTPDebug = 2;                                 // Enable verbose debug output
             $mail->isSMTP();                                      // Set mailer to use SMTP
             $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
@@ -52,13 +52,13 @@ require 'vendor/autoload.php';
             $mail->Port = 587;                                    // TCP port to connect to
 
             //Recipients
-            $mail->setFrom($header);
+            $mail->setFrom('no-reply@rfpdeployment.com');
             $mail->addAddress('rizkyfalihpridyandhika@gmail.com');               // Name is optional
 
             //Content
             $mail->isHTML(true);                                  // Set email format to HTML
             $mail->Subject = $subject;
-            $mail->Body    = $body;
+            $mail->Body    = 'From: ' . $header . '<br><br>' . $body;
            
             $mail->send();
             echo 'Message has been sent';
