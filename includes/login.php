ce7f0390185d1531fade79d675fb1f1b85f090c5
rizkyfalih <rizkyfalihpridyandhika@ymail.com>
2018-08-08 08:27:06 +0700

    updated

--- includes/login.php
+++ includes/login.php
@@ -6,41 +6,7 @@
 <?php
 
     if(isset($_POST['login'])){
-        // login_user($_POST['username'],$_POST['password']);
-   
-        $username = trim($username);
-        $password = trim($password);
-    
-        $username = mysqli_real_escape_string($connection, $username);
-        $password = mysqli_real_escape_string($connection, $password);
-    
-        $query = "SELECT * FROM users WHERE username = '{$username}' ";
-        $select_user_query = mysqli_query($connection, $query);
-        if(!$select_user_query){
-            die("QUERY FAILED" . mysqli_error($connection));
-        }
-    
-        while($row = mysqli_fetch_array($select_user_query)){
-            $db_user_id = $row['user_id'];
-            $db_username = $row['username'];
-            $db_user_password = $row['user_password'];
-            $db_user_firstname = $row['user_firstname'];
-            $db_user_lastname = $row['user_lastname'];
-            $db_user_role = $row['user_role'];
-    
-        }
-    
-    
-        if(password_verify($password, $db_user_password) && $username === $db_username){
-            $_SESSION['username'] = $db_username;
-            $_SESSION['firstname'] = $db_user_firstname;
-            $_SESSION['lastname'] = $db_user_lastname;
-            $_SESSION['user_role'] = $db_user_role;
-            
-            return header("Location: admin/");
-        } else {
-            echo "Gagal";
-        }
+        login_user($_POST['username'],$_POST['password']);
    
     }
 
